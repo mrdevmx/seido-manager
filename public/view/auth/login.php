29e6c6a5d484ecad4d1d3eb425e343eecfd91a71
Alberto Martinez <alberto.martinez@mrdev.mx>
2024-08-31 15:59:35 -0500

    Ajustando enrutamiento

--- public/view/auth/login.php
+++ public/view/auth/login.php
@@ -1,7 +1,16 @@
 <?php 
     session_start(); 
     if(isset($_SESSION['userid'])){ 
-        header('Location: ./ ');
+        $userid = $_SESSION['userid'];
+        $username = $_SESSION['username'];
+        $shortname = $_SESSION['shortname'];
+	    $shortlastname = $_SESSION['shortlastname'];
+        $permisos = $_SESSION['tipous'];
+        $company = $_SESSION['company'];
+        $pathprincipal = $_SESSION['path'];
+    
+    
+        header('Location: ./'.$pathprincipal);
     }
 ?>
 <!DOCTYPE html>
