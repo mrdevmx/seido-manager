f21f36404f0c38822978f049af6b6350559427ca
Alberto Martinez <alberto.martinez@mrdev.mx>
2025-02-06 17:30:33 -0500

    agregando a env key secret

--- auth/en-uncrypt.php
+++ auth/en-uncrypt.php
@@ -1,20 +1,20 @@
 <?php
-    function encrypt($data){
-        $hash=password_hash($data, PASSWORD_DEFAULT);
-        $mitad = strlen($hash ) / 2; //Cantidad de letras en $nombre dividida entre 2 
-        $parte1 = substr($hash , 0, $mitad); 
-        $parte2 = substr($hash , $mitad); 
+function encrypt($data){
+    $hash=password_hash($data, PASSWORD_DEFAULT);
+    $mitad = strlen($hash ) / 2; //Cantidad de letras en $nombre dividida entre 2
+    $parte1 = substr($hash , 0, $mitad);
+    $parte2 = substr($hash , $mitad);
 
-        $pass = $parte1."MaRj".$parte2;
-        $passCrypt = rtrim(strtr(base64_encode($pass), '+/', '-_'), '=');
+    $pass = $parte1.$_ENV['KEY_SECRET'].$parte2;
+    $passCrypt = rtrim(strtr(base64_encode($pass), '+/', '-_'), '=');
 
-        return $passCrypt;
-    }
+    return $passCrypt;
+}
 
-    function uncrypt($passEncrypt){
-        $uncrypt = base64_decode(str_pad(strtr($passEncrypt, '-_', '+/'), strlen($passEncrypt) % 4, '=', STR_PAD_RIGHT));
-        $passUncrypt = str_replace("MaRj", "", $uncrypt);
+function uncrypt($passEncrypt){
+    $uncrypt = base64_decode(str_pad(strtr($passEncrypt, '-_', '+/'), strlen($passEncrypt) % 4, '=', STR_PAD_RIGHT));
+    $passUncrypt = str_replace($_ENV['KEY_SECRET'], "", $uncrypt);
 
-        return $passUncrypt;
-    }
+    return $passUncrypt;
+}
 ?>
